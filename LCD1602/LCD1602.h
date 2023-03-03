bff5c2f49e83b02e6e7f0f89901f2d29a80f25c6
Jingqing3948 <2351290287@qq.com>
2023-03-03 08:37:29 +0800

    modify LCD1602.h

--- LCD1602/LCD1602.h
+++ LCD1602/LCD1602.h
@@ -1,6 +1,13 @@
 #ifndef __LCD1602_H__
 #define __LCD1602_H__
 
+#include <REGX52.H>
+//set pin
+sbit LCD_RS=P2^6;
+sbit LCD_RW=P2^5;
+sbit LCD_EN=P2^7;
+#define LCD_DataPort P0
+
 //用户调用函数：
 void LCD_Init();
 void LCD_ShowChar(unsigned char Line,unsigned char Column,char Char);
