3efc79715ce4b1a4b5d684adec6794bf569faf87
Jingqing3948 <2351290287@qq.com>
2023-03-03 08:19:39 +0800

    add key.c comment

--- Key/Key.c
+++ Key/Key.c
@@ -1,39 +1,42 @@
+/*
+ * @Author: Jingqing3948 2351290287@qq.com
+ * @Date: 2023-02-08 01:08:08
+ * @LastEditors: Jingqing3948 2351290287@qq.com
+ * @LastEditTime: 2023-03-03 08:08:20
+ * @Description: control 4 independent key.
+ *
+ * Copyright (c) 2023 by Jingqing3948 2351290287@qq.com, All Rights Reserved.
+ */
 #include "reg52.h"
 #include "Key.h"
+#include "Delay.h"
 
-
-void delay_10us(u16 ten_us)
+/**
+ * @brief: scan which key is pressed, and return it.
+ * @param {u8} mode
+ * @return {*}
+ */
+unsigned char key_scan(unsigned char mode)
 {
-while(ten_us--);
-}
-
-u8 key_scan(u8 mode)
-{
-	static u8 key=1;
-	if(mode)key=1;//连续扫描按键
-	if(key==1&&(KEY1==0||KEY2==0||KEY3==0||KEY4==0))//任意按键按下
+	static unsigned char key = 1;
+	if (mode)//continuous scan. If press key for a while, it represents this key has been pressed for many times.
+		key = 1;
+	if (key == 1 && (KEY1 == 0 || KEY2 == 0 || KEY3 == 0 || KEY4 == 0)) // 锟斤拷锟解按锟斤拷锟斤拷锟斤拷
 	{
-		delay_10us(1000);//10ms 消抖
-        /*另一种消抖的方法：
-        if(KEY==0){
-        	delay_ms(20);
-        	while(KEY==0);
-        	delay(20ms);
-        }
-        这个是针对按下按钮又抬起按钮之后的执行。*/
-		key=0;
-		if(KEY1==0)
-		return KEY1_PRESS;
-		else if(KEY2==0)
-		return KEY2_PRESS;
-		else if(KEY3==0)
-		return KEY3_PRESS;
-		else if(KEY4==0)
-		return KEY4_PRESS;
+		delayMs(10); // 10ms elimination buffeting of keystroke
+		key = 0;
+		if (KEY1 == 0)
+			return KEY1_PRESS;
+		else if (KEY2 == 0)
+			return KEY2_PRESS;
+		else if (KEY3 == 0)
+			return KEY3_PRESS;
+		else if (KEY4 == 0)
+			return KEY4_PRESS;
 	}
-	else if(KEY1==1&&KEY2==1&&KEY3==1&&KEY4==1) //无按键按下
+	else if (KEY1 == 1 && KEY2 == 1 && KEY3 == 1 && KEY4 == 1) // no key pressed
 	{
-		key=1;
+		key = 1;
 	}
 	return KEY_UNPRESS;
 }
