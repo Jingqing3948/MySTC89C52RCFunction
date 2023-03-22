fa8adfdea5eb04aa35c58c7d4c25cd0e5352fe02
Jingqing3948 <94222301+Jingqing3948@users.noreply.github.com>
2023-03-22 21:26:57 +0800

    modify comment

--- Key/Key.h
+++ Key/Key.h
@@ -1,17 +1,16 @@
 #include "reg52.h"
-typedef unsigned int u16; //对系统默认数据类型进行重定义
-typedef unsigned char u8;
-//定义独立按键控制脚
+#include "public.h"
+//individual button control pin definition
 sbit KEY1=P3^1;
 sbit KEY2=P3^0;
 sbit KEY3=P3^2;
 sbit KEY4=P3^3;
-//定义 LED1 控制脚
+//LED control pin definition
 sbit LED1=P2^0;
 sbit LED2=P2^1;
 sbit LED3=P2^2;
 sbit LED4=P2^3;
-//使用宏定义独立按键按下的键值
+//key value macro definition
 #define KEY1_PRESS 1
 #define KEY2_PRESS 2
 #define KEY3_PRESS 3
